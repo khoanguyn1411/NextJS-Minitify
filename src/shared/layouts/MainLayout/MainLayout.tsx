c1b4e7c7f72f2b254a2b558d8b871f78c3b380fb
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-08 22:20:42 +0700

    Config tailwind css

--- src/shared/layouts/MainLayout/MainLayout.tsx
+++ src/shared/layouts/MainLayout/MainLayout.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@nextui-org/button";
-import { Input, NextUIProvider } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 import Link from "next/link";
 import { type FC, type PropsWithChildren } from "react";
 
 export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <NextUIProvider>
-      <h1>This is main layout</h1>
+    <div>
+      <h1 className="bg-black text-red-600">This is main layout</h1>
 
       <Button>This is test button</Button>
       <Input/>
@@ -15,6 +15,6 @@ export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
       <Link href={"/trending-musics"}>Trending music</Link>
       <Link href={"/"}>Home</Link>
       {children}
-    </NextUIProvider>
+    </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { nextui } = require("@nextui-org/theme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
