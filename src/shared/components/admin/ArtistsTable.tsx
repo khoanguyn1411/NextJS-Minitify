35bce7fb405a0da7fcc47df3047d7666081780ee
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-14 22:00:14 +0700

    Add artist table

--- src/shared/components/admin/ArtistsTable.tsx
+++ src/shared/components/admin/ArtistsTable.tsx
@@ -1,5 +1,25 @@
-import { type FC } from "react";
+"use client";
+
+import { type Artist } from "@prisma/client";
+import { useEffect, useState, type FC } from "react";
+
+import { getArtists } from "@/core/apis/artistApis";
+import { BaseFilterParams } from "@/core/models/baseFilterParams";
+import { type Pagination } from "@/core/models/pagination";
 
 export const ArtistsTable: FC = () => {
-  return <div>ArtistsTable</div>;
+  const [artistsPage, setArtistsPage] = useState<Pagination<Artist> | null>(
+    null,
+  );
+
+  const fetchArtistPage = async () => {
+    const page = await getArtists(BaseFilterParams.initialPagination);
+    setArtistsPage(page);
+  };
+
+  useEffect(() => {
+    fetchArtistPage();
+  }, []);
+
+  return <div>{artistsPage?.totalCount}</div>;
 };
