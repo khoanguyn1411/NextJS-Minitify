c2813797acf46c2ff19c72be78f22b6229fd0630
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-14 12:08:26 +0700

    Improve navigation style

--- src/shared/layouts/navigationAside/NavigationAside.tsx
+++ src/shared/layouts/navigationAside/NavigationAside.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link, type NavbarItem } from "@nextui-org/react";
+import { Divider, Link, type NavbarItem } from "@nextui-org/react";
 import { type FC, type ReactNode } from "react";
 import { BiLibrary, BiMusic, BiTrendingUp, BiWorld } from "react-icons/bi";
 
@@ -35,18 +35,25 @@ const navbarItems: readonly NavbarItem[] = [
 
 export const NavigationAside: FC = () => {
   return (
-    <div className="flex flex-col py-container">
-      {navbarItems.map((item) => (
-        <Link
-          className="flex gap-3 py-2 px-container hover:text-primary-100 w-full self-start"
-          key={item.url}
-          color="foreground"
-          href={item.url}
-        >
-          {item.icon}
-          {item.title}
-        </Link>
-      ))}
+    <div className="flex flex-col p-container gap-3 h-full">
+      <div className="flex flex-col">
+        {navbarItems.map((item) => (
+          <Link
+            className="flex gap-3 py-2 hover:text-primary-100 w-full self-start"
+            key={item.url}
+            color="foreground"
+            href={item.url}
+          >
+            {item.icon}
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <Divider />
+      <div className="mt-auto">
+        <p className="text-sm">This is self-edu project.</p>
+        <p className="text-xs">No commercial purpose.</p>
+      </div>
     </div>
   );
 };
