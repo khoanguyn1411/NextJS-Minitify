5e84659032f054c4b0abdbd369ab5b4f6ae6736d
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-14 12:01:42 +0700

    Build left sidebar

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -9,12 +9,16 @@ export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
       <header className="p-container">
         <Header />
       </header>
-      <main className="h-full grid grid-cols-4">
-        <aside className="p-container">
+      <main className="h-full grid grid-cols-6 px-2 gap-2">
+        <aside className="h-full bg-primary-900 rounded-md">
           <NavigationAside />
         </aside>
-        <div className="p-container col-span-2">{children}</div>
-        <aside className="p-container">This is aside right</aside>
+        <div className="p-container col-span-4 max-h-full h-full bg-primary-900 rounded-md">
+          {children}
+        </div>
+        <aside className="p-container h-full bg-primary-900 rounded-md">
+          This is aside right
+        </aside>
       </main>
       <footer className="mt-auto p-container">This is footer</footer>
     </div>
--- src/shared/layouts/navigationAside/NavigationAside.tsx
+++ src/shared/layouts/navigationAside/NavigationAside.tsx
@@ -1,31 +1,52 @@
 "use client";
 
-import { Navbar, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
-import { useState, type FC } from "react";
+import { Link, type NavbarItem } from "@nextui-org/react";
+import { type FC, type ReactNode } from "react";
+import { BiLibrary, BiMusic, BiTrendingUp, BiWorld } from "react-icons/bi";
+
+type NavbarItem = {
+  readonly title: string;
+  readonly url: string;
+  readonly icon: ReactNode;
+};
+
+const navbarItems: readonly NavbarItem[] = [
+  {
+    title: "Library",
+    url: "/library",
+    icon: <BiLibrary />,
+  },
+  {
+    title: "Trending Musics",
+    url: "/trending-musics",
+    icon: <BiTrendingUp />,
+  },
+  {
+    title: "Discover",
+    url: "/discover",
+    icon: <BiWorld />,
+  },
+  {
+    title: "New songs",
+    url: "/new-songs",
+    icon: <BiMusic />,
+  },
+];
 
 export const NavigationAside: FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
   return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>{item}</NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+    <div className="flex flex-col py-container">
+      {navbarItems.map((item) => (
+        <Link
+          className="flex gap-3 py-2 px-container hover:text-primary-100 w-full self-start"
+          key={item.url}
+          color="foreground"
+          href={item.url}
+        >
+          {item.icon}
+          {item.title}
+        </Link>
+      ))}
+    </div>
   );
 };
