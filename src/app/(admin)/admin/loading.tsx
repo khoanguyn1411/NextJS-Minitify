77f1bd47d23d2cde02c841828fec20a46b302eec
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-16 22:36:09 +0700

    Change to input color for loading

--- src/app/(admin)/admin/loading.tsx
+++ src/app/(admin)/admin/loading.tsx
@@ -1,10 +1,10 @@
 const LoadingSkeleton = () => (
   <div className="space-y-5">
-    <div className="h-24 rounded-lg bg-primary-200"></div>
+    <div className="h-24 rounded-lg bg-input"></div>
     <div className="space-y-3">
-      <div className="h-3 w-3/5 rounded-lg bg-primary-200"></div>
-      <div className="h-3 w-4/5 rounded-lg bg-primary-200"></div>
-      <div className="h-3 w-2/5 rounded-lg bg-primary-200"></div>
+      <div className="h-3 w-3/5 rounded-lg bg-input"></div>
+      <div className="h-3 w-4/5 rounded-lg bg-input"></div>
+      <div className="h-3 w-2/5 rounded-lg bg-input"></div>
     </div>
   </div>
 );
