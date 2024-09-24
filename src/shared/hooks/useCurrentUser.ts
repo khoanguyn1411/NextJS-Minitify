b451dfc6a0749d728536d3f52e046b2a650183c1
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-24 19:47:57 +0700

    Add docs

--- src/shared/hooks/useCurrentUser.ts
+++ src/shared/hooks/useCurrentUser.ts
@@ -2,6 +2,7 @@ import { type User } from "@prisma/client";
 import { createContext, useContext } from "react";
 export const UserContext = createContext<User | null>(null);
 
+/** Get current user. Only available with users app. For admin app, please use validateRequest. */
 export const useCurrentUser = () => {
   const currentUser = useContext(UserContext);
   return {
