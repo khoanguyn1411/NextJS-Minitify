c6d0cd9f582b2b5fc89c2271818e549ceeb75306
khoanguyn1411 <khoaah1411@gmail.com>
2024-09-28 18:35:53 +0700

    Move hoamge page to users group

---
+++ src/app/(users)/page.tsx
@@ -0,0 +1,22 @@
+import { type Metadata } from "next";
+
+import { ChillingSongs } from "@/shared/components/home/ChillingSongs";
+import { FamousArtists } from "@/shared/components/home/FamousArtists";
+import { ForYou } from "@/shared/components/home/ForYou";
+import { HotAlbums } from "@/shared/components/home/HotAlbums";
+
+export const metadata: Metadata = {
+  title: "Minitify",
+  description: "To beautiful musics world.",
+};
+
+export default async function Home() {
+  return (
+    <div className="flex flex-col gap-2">
+      <ForYou />
+      <ChillingSongs />
+      <HotAlbums />
+      <FamousArtists />
+    </div>
+  );
+}
